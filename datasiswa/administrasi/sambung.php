cb365311cbc44cdc9dfa11f7bc74d0a9490d7432
aragita <ara.gita@student.unsil.ac.id>
2014-10-20 20:55:55 +0700

    Revert "e-2012"
    This reverts commit 035ac9e3b25171aa307971aedb03760bade01abd.

--- datasiswa/administrasi/sambung.php
+++
@@ -1,8 +0,0 @@
-<?php
-$host="localhost";
-$userdb="madcoms";
-$passdb="3266";
-$namadb="administrasi";
-$sambung=mysql_connect($host,$userdb,$passdb);
-mysql_select_db($namadb,$sambung);
-?>
--- datasiswa/instruktur/sambung.php
+++
@@ -1,8 +0,0 @@
-<?php
-$host="localhost";
-$userdb="root";
-$passdb="";
-$namadb="administrasi";
-$sambung=mysql_connect($host,$userdb,$passdb);
-mysql_select_db($namadb,$sambung);
-?>
